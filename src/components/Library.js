d7cafb087213340d0f8250e63363555ed2e359e5
Rishi <l.rishissahu@gmail.com>
2023-06-01 01:10:04 +0530

    songs are changing from library.

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,13 +1,13 @@
 import React from "react";
 import LibrarySong from "./LibrarySong";
 
-const Library = ({ songs }) => {
+const Library = ({ songs,setCurrentSong }) => {
     return (
         <div className="library">
             <h1>Library</h1>
             <div className="library-songs">
                 {songs.map((song) => (
-                    <LibrarySong key={song.id} song={song} />
+                    <LibrarySong key={song.id} song={song} setCurrentSong={setCurrentSong} />
                 ))}
             </div>
         </div>
--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,15 +1,19 @@
 import React from "react";
 
+const LibrarySong = ({ song,setCurrentSong }) => {
+    const handleClick = () => {
+        setCurrentSong(song);
+    };
 
-const LibrarySong = ({ song }) => {
     return (
-        <div className="lib-song-container">
+        <div onClick={handleClick} className="lib-song-container">
             <img src={song.artwork} alt="" />
             <div className="lib-song-description">
                 <h3>{song.title}</h3>
                 <h5>{song.artist}</h5>
             </div>
         </div>
-    )
-}
-export default LibrarySong
+    );
+};
+
+export default LibrarySong;
